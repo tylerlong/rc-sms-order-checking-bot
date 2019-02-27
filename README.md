574bb368874dc85c1c75764bd0fface19974b40d
Tyler Long <tyler4long@gmail.com>
2019-02-27 16:40:49 +0800

    First working version

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
 # RingCentral SMS order checking bot
 
-## Setup
+## Setup the project
 
 ```
+yarn install
 cp .env.sample .env
 ```
 
 Edit .env to specify credentials
+
+
+## Start the bot
+
+```
+yarn start
+```
+
+
+## Test the bot
+
+Send sms to the phone number you configured in `.env` and talk to the bot.
+
+
+## Sampe conversation
+
+```
+Me: Hi
+Bot: Hi, how can I help?
+Me: When is my order arriving?
+Bot: What is your order ID number?
+Me: 1234567 Bot: Our records show that your order should arrive on Friday by noon. Is there anything else I can help with?
+Me: Nope, thanks!
+Bot: Have a great day.
+```
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const RingCentral = require('ringcentral-js-concise').default
+const Pubnub = require('ringcentral-js-concise/dist/pubnub').default
+const delay = require('timeout-as-promise')
+const moment = require('moment')
 
 const rc = new RingCentral(
   process.env.RINGCENTRAL_CLIENT_ID,
@@ -6,14 +9,50 @@ const rc = new RingCentral(
   process.env.RINGCENTRAL_SERVER_URL
 )
 
+const pubnub = new Pubnub(rc, ['/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS'], message => {
+  handleMessage(message)
+})
+
+const handleMessage = message => {
+  if (message.body && message.body.subject) {
+    console.log(`Got message from customer: ${message.body.subject}`)
+    const reply = getReply(message.body.subject)
+    if (reply) {
+      console.log(`Replying to customer: ${reply}`)
+      rc.post('/restapi/v1.0/account/~/extension/~/sms', {
+        to: [{ phoneNumber: message.body.from.phoneNumber }],
+        from: { phoneNumber: process.env.RINGCENTRAL_USERNAME },
+        text: reply
+      })
+    }
+  }
+}
+
+const getReply = subject => {
+  const input = subject.trim().toLowerCase()
+  if (input.startsWith('hi') || input.startsWith('hello')) {
+    return 'Hi, how can I help?'
+  } else if (input.includes('order') && input.includes('arriv')) {
+    return 'What is your order ID number?'
+  } else if (/\d{3,20}/.test(input)) {
+    return `Our records show that your order should arrive on ${moment().add(3, 'days').format('ddd, Do MMMM')} by noon. Is there anything else I can help with?`
+  } else if (input.startsWith('no') || input.startsWith('nope')) {
+    return 'Have a great day!'
+  } else {
+    return undefined
+  }
+}
+
 const main = async () => {
   await rc.authorize({
     username: process.env.RINGCENTRAL_USERNAME,
     extension: process.env.RINGCENTRAL_EXTENSION,
     password: process.env.RINGCENTRAL_PASSWORD
   })
-  console.log(rc.token())
+
+  await pubnub.subscribe()
+
+  await delay(3600000)
   await rc.revoke()
 }
-
 main()
